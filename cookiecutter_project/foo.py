294227e5ea1930ab9317a64a0b639b46145a264a
Alex Raum <alexraum89@gmail.com>
2024-02-10 21:43:57 -0500

    Address formatting issues

--- cookiecutter_project/foo.py
+++ cookiecutter_project/foo.py
@@ -4,9 +4,11 @@ def foo() -> str:
     Extended description of function.
 
     Args:
+    ----
         foo (str): Description of arg1
 
     Returns:
+    -------
         str: Description of return value
     """
 
